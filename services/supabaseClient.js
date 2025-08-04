c6d973b4d44495e34e236f71bdb77e964ab77af1
Kimathi Sedegah <sedkim370@gmail.com>
2025-08-04 22:20:56 +0000

    Update supabaseClient.js

--- services/supabaseClient.js
+++ services/supabaseClient.js
@@ -1,11 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || process.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_KEY || process.env.VITE_SUPABASE_KEY;
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseAnonKey = process.env.SUPABASE_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
-  console.error('Supabase environment variables are missing.');
-  throw new Error('Missing Supabase environment variables! Check Vercel or .env file.');
+  throw new Error("Missing Supabase environment variables! Check Vercel or .env file.");
 }
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
