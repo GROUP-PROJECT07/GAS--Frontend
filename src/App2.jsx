63d10c7b7957b3d3c6bc09f711735543b039adb2
Kimathi Sedegah <sedkim370@gmail.com>
2025-08-08 00:34:11 +0000

    Update App2.jsx

--- src/App2.jsx
+++ src/App2.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import supabase from "./services/supabaseClient";
+import { supabase } from "./services/supabaseClient"; 
 import AuthForm from "./AuthForm";
 import App2 from "./App2";
 
@@ -177,3 +177,4 @@ function MainApp() {
 }
 
 export default MainApp;
+
