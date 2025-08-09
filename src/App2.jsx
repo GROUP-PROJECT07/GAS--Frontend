e0868fcc0adb553b697d0ee558c944c9519a0c08
Kimathi Sedegah <sedkim370@gmail.com>
2025-08-09 10:53:09 +0000

    Add vercel.json and update routing setup

--- src/App2.jsx
+++ src/App2.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import supabase from "./services/supabaseClient";
 import AuthForm from "./AuthForm";
 import App2 from "./App2";
